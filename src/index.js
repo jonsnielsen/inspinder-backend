692799c8567428d03d74ab20fe1bc6c66d32f5c6
Jonathan Sparvath <jonathan.sparvath@gmail.com>
2019-01-18 13:40:10 +0100

    enabled cors

--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
-const cookieParser = require("cookie-parser")();
-const jwt = require("jsonwebtoken");
-require("dotenv").config({ path: "variables.env" });
-const createServer = require("./createServer");
-const db = require("./db");
+const cookieParser = require('cookie-parser')();
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+const cors = require('cors');
 
 const server = createServer();
 
 server.express.use(cookieParser);
-
+server.express.use(cors());
 //decode the JWT so we can get the user id on each request
 server.express.use((req, res, next) => {
-  const { token } = req.cookies;
-  if (token) {
-    const { userId } = jwt.verify(token, process.env.APP_SECRET);
-    // put the user id on the request for further requests to accress
-    req.userId = userId;
-  }
-  next();
+	const { token } = req.cookies;
+	if (token) {
+		const { userId } = jwt.verify(token, process.env.APP_SECRET);
+		// put the user id on the request for further requests to accress
+		req.userId = userId;
+	}
+	next();
 });
 
 //decode the JWT so we can get the user id on each request
 server.express.use(async (req, res, next) => {
-  if(!req.userId) {
-    return next ();
-  }
-  const user = await db.query.user({where:{id: req.userId}}, '{id, permissions,email,name}')
-  req.user = user;
-  next();
+	if (!req.userId) {
+		return next();
+	}
+	const user = await db.query.user({ where: { id: req.userId } }, '{id, permissions,email,name}');
+	req.user = user;
+	next();
 });
-
 
 //start the server
 server.start(
-  {
-    cors: {
-      credentials: true,
-      origin: process.env.FRONTEND_URL
-    }
-  },
-  deets => {
-    console.log(`Server is now running on port http://localhost:${deets.port}`);
-  }
+	// {
+	// 	cors: {
+	// 		credentials: true,
+	// 		origin: process.env.FRONTEND_URL
+	// 	}
+	// },
+	(deets) => {
+		// console.log(`Server is now running on port http://localhost:${deets.port}`);
+	}
 );
